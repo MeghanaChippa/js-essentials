3c7ce4f5f09402fa21d66e738d204ae8a3c528b9
meghana chippa <meghanachippa@meghanas-MacBook-Pro.local>
2025-04-12 15:17:15 +0530

    Added search

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Travel website loaded!");
+  function handleSearch() {
+    const value = document.getElementById("searchInput").value.trim();
+    if (value) {
+     
+    }
+  }
+
+  function clearSearch() {
+    document.getElementById("searchInput").value = "";
+  }
